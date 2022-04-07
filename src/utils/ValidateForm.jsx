de2e46358fafe628e86b1c97bdecfb68af13db05
apzaripova <ap.zaripova@gmail.com>
2022-04-07 12:04:10 +0300

    proverka

--- src/utils/ValidateForm.jsx
+++ src/utils/ValidateForm.jsx
@@ -7,9 +7,9 @@ export function useFormValidation() {
   const [isFormValid, setIsFormValid] = React.useState(false);
 
   const handleChange = (event) => {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
+    const {target} = event;
+    const {name} = target;
+    const {value} = target;
     setValues({...values, [name]: value});
     setErrors({...errors, [name]: target.validationMessage });
     setValidity({...validity, [name]: target.validity.valid })
