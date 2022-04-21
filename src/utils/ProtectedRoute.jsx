9923a0ea6a47b9f53aace11f43fadef717a2897c
apzaripova <ap.zaripova@gmail.com>
2022-04-21 19:38:26 +0300

    proverka loggedIn

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -6,7 +6,7 @@ const ProtectedRoute = ({ component: Component, ...props  }) => {
   return (
     <Route>
       {
-        () => props.loggedIn ? <Component {...props} /> : <Redirect to="./" />
+        () => props.loggedIn ? <Component {...props} /> : <Redirect to="/" />
       }
     </Route>
 )}
